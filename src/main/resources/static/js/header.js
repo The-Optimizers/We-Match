668133aab0376a2857af548e753734671b0d0122
GHADEER KHASAWNEH <GHADEER_KHASAWNEH@YAHOO.COM>
2021-10-25 22:05:18 +0300

    Working on design with Sanaa

---
+++ src/main/resources/static/js/header.js
@@ -0,0 +1,10 @@
+
+//<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+//
+//<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+//
+//    window.addEventListener("scroll", () => {
+//  let header = document.querySelector(".header");
+//  header.classList.toggle("sticky", window.scrollY > 0);
+//});
+
